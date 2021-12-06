b39baa32025554e2542b43d4db705bca127b66c5
amichaikafka <amichaikp@gmail.com>
2021-12-06 13:51:04 +0200

    second

--- main.py
+++ main.py
@@ -10,15 +10,3 @@ class Distibution:
         return 1/(self.max-self.min)
     def r(self,x:int)->float:
         return x-((1-self.F(x))/self.div_F())
-if __name__ == '__main__':
-    l=[10,30,30,30]
-    d=Distibution(l)
-    print(d.div_F(),list(map(lambda x:(d.F(x),d.r(x),d.div_F()),l)))
-    l=[50,5,29]
-    print(d.div_F(), list(map(lambda x: (d.F(x), d.r(x), d.div_F()), l)))
-    l=[i for i in range(10,30,2)]
-    print(d.div_F(), list(map(lambda x: (x,d.F(x), d.r(x), d.div_F()), l)))
-    l = [10, 30]
-    d = Distibution(l)
-    print(d.div_F(), list(map(lambda x: (x, d.F(x), d.r(x), d.div_F()), l)))
-    print(d.F(12))
