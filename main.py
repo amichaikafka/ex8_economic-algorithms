e587db7530c941f64c9af0a1a27e4e29eb5e6143
amichaikafka <amichaikp@gmail.com>
2021-12-06 23:53:53 +0200

    finish

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ class Distibution:
         self.values=values
         self.max=max(self.values)
         self.min=min(self.values)
+        self.d=dict.fromkeys(values,0)
+        for i in values:
+            self.d[i]+=1
 
     def F(self,x:int)->float:
-        return (x-self.min)/(self.max-self.min)
+        sum=0
+        for i,j in self.d.items():
+            if i <x:
+                sum+=j
+        return sum/len(self.values)
+        # return (x-self.min)/(self.max-self.min)
     def div_F(self)->float:
         return 1/(self.max-self.min)
     def r(self,x:int)->float:
         return x-((1-self.F(x))/self.div_F())
+
